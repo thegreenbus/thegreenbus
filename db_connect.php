bc67edb011bc3f5ae148e43145d1280ed37d77f4
georgethedeveloper <georgekarani77@gmail.com>
2022-05-31 15:49:59 +0300

    db

--- db_connect.php
+++ db_connect.php
@@ -1,4 +1,8 @@
 <?php
 $conn= new mysqli('localhost','root','','bus_booking')or die("Could not connect to mysql".mysqli_error($conn));
-
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
 ?>
